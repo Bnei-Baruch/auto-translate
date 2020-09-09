6caf5efa9379523fbf1803ce39d45df42dd95b27
Yuri Rapoport <yuri.rapoport@gmail.com>
2020-09-09 20:19:50 +0300

    add download scripts

---
+++ zohar_download_article.py
@@ -0,0 +1,137 @@
+import requests
+import json
+import lxml.html
+
+import argparse
+import os
+from pathlib import Path
+
+# Base url of articles in different languages
+SOURCE_BASE_URL = 'https://kabbalahmedia.info/{lang}/sources/'
+
+# Part of the url which appears before article id
+BEFORE_ID = 'sources/'
+
+# Base url of the actual docx files
+ASSET_BASE_URL = 'https://kabbalahmedia.info/assets/sources/'
+
+# Sample url with tree links
+SAMPLE_URL = 'https://kabbalahmedia.info/he/sources/yUcfylRm'
+
+# The documents in these languages will be downloaded:
+LANGS = ('en', 'he')
+
+# Documents format
+FORMAT = 'docx'
+
+class HtmlFormatChanged(Exception):
+    pass
+
+class MissingLanguage(Exception):
+    def __init__(self, article_id, lang):
+        super().__init__(lang)
+        self.article_id = article_id
+        self.lang = lang
+
+class NoArticleId(Exception):
+    def __init__(self, url):
+        super().__init__("Could not extract article id from url " + url)
+
+class Asset:
+    "Web page content and title"
+
+    def __init__(self, content, title, lang):
+        self.content = content
+        self.title = title
+        self.lang = lang
+
+    def formatted_title(self, _id):
+        formatted = self.title.split('|')[0].split('-')[0].strip().replace(' ', '-')
+        return f'{formatted}-{_id}'
+        
+def lang_links(txt):
+    """This function extracts links per language part from a webpage.
+
+    >>> js = 'var obj = {"data":{"he": "http://addr1", "en": "http://addr2"}};'
+    >>> lang_detals(obj)
+    '{"he": "http://addr1", "en": "http://addr2"}'
+    """
+
+    START_HINT = '"data":{'
+    
+    start = txt.find(START_HINT)
+    if start < 0:
+        raise HtmlFormatChanged("Data start hint not found")
+
+    start += len(START_HINT)
+    balance = 1
+    for i in range(start, len(txt)):
+        balance += (txt[i] == '{') - (txt[i] == '}')
+        if balance == 0:
+            return txt[start - 1:i + 1]
+    
+    raise HtmlFormatChanged("Unblanced data section")
+
+def load_assets(article_id, langs=LANGS, fmt=FORMAT):
+    "Downloads article with given id for given languages"
+
+    assert langs, 'languages list cannot be empty'
+
+    source_url = SOURCE_BASE_URL.format(lang=langs[0]) + article_id
+    src = requests.get(source_url)
+    title = lxml.html.fromstring(src.text).findtext('.//title')
+    
+    links = json.loads(lang_links(src.text))
+    for lang in langs:
+        if lang not in links:
+            raise MissingLanguage(article_id, lang)
+    
+        url = ASSET_BASE_URL + article_id + '/' + links[lang][fmt]
+        yield Asset(requests.get(url).content, title, lang)
+
+def extract_id(asset_url):
+    """Extracts document id from url.
+
+    >>> extract_id('https://kabbalahmedia.info/he/sources/6WPLdxTX?language=en')
+    '6WPLdxTX'
+    """
+    i = asset_url.find(BEFORE_ID)
+    if i < 0:
+        return NoArticleId(asset_url)
+    
+    return asset_url[i + len(BEFORE_ID):].split('?')[0]
+
+def file_size(path):
+    try:
+        return Path(path).stat().st_size
+    except FileNotFoundError:
+        return -1
+
+def download(_id, dest=''):
+    assets = list(load_assets(_id))
+
+    assert assets, "documents number must be equal to LANGS length"
+
+    folder = assets[0].formatted_title(_id)
+    base = os.path.join(dest, folder)
+    os.makedirs(base, exist_ok=True)
+    
+    for asset in assets:
+        path = os.path.join(base, asset.lang) + '.' + FORMAT
+        if file_size(path) == len(asset.content):
+            continue
+
+        with open(path, 'wb') as f:
+            f.write(asset.content)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url")
+
+    args = parser.parse_args()
+ 
+    _id = extract_id(url)
+    download(_id)
+
+if __name__ == "__main__":
+    main()
---
+++ zohar_download_root.py
@@ -0,0 +1,42 @@
+import requests
+import re
+import argparse
+import tqdm
+import sys
+
+from zohar_download_article import download, MissingLanguage
+
+SAMPLE_URL = 'https://kabbalahmedia.info/he/sources/yUcfylRm'
+LINK_REGEX = re.compile(r'div id\=\"title\-[A-Za-z0-9]+\"')
+
+def sources_list(base=SAMPLE_URL):
+    LINK_REGEX = re.compile(r'div id\=\"title\-[A-Za-z0-9]+\"')
+    
+    src = requests.get(base).text
+    links = LINK_REGEX.findall(src)
+    
+    return list({link[len('div id="title-'):-1] for link in links})
+
+def guarded_download(src_dest):
+    src, dest = src_dest
+    try:
+        download(src, dest)
+    except MissingLanguage:
+        pass
+    except Exception as e:
+        print('Failed downloading', src, file=sys.stderr)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--root", default=SAMPLE_URL)
+    parser.add_argument("--dest", default='zohar/')
+
+    args = parser.parse_args()
+    sources = sources_list(args.root)
+
+    progress = tqdm.tqdm(range(len(sources)))
+    for src, _ in zip(sources, progress):
+        guarded_download((src, args.dest))
+
+if __name__ == "__main__":
+    main()
