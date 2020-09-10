7337bd960fd8b6b15991d1f3f9729d8099a44b4d
Yuri Rapoport <yuri.rapoport@gmail.com>
2020-09-10 14:57:18 +0300

    add diff debug tool (diffmaker)

---
+++ diffmaker.py
@@ -0,0 +1,50 @@
+import argparse
+from summary import letters_chunks
+
+import sys
+
+def diff(eng, heb):
+    letters = letters_chunks(eng, heb)
+    print('<html><head><title>difftool</title></head><body>')
+
+    for letter, en, he in letters:
+        en_chunks = en.strip().split('\n') if en else ()
+        he_chunks = he.strip().split('\n') if he else ()
+
+        engen = (chunk for chunk in en_chunks)
+        hegen = (chunk for chunk in he_chunks)
+
+        print('<table border="5"><tr><th colspan=2>', letter, '</th></tr>')
+        for en_txt, he_txt in zip(engen, hegen):
+            print('<tr>')
+            print('<td style="text-direction: ltr; width: 50%">')
+            print(en_txt)
+            print('</td><td style="text-direction: rtl">')
+            print(he_txt)
+            print('</td></tr>')
+
+        for en_txt in engen:
+            print('<tr><td style="text-direction: ltr; width: 50%">')
+            print(en_txt)
+            print('</td><td></td></tr>')
+
+        for he_txt in hegen:
+            print('<tr><td></td><td style="text-direction: rtl">')
+            print(he_txt)
+            print('</td></tr>')
+    print('</table>')
+
+    print('</body></html>')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("english_file")
+    parser.add_argument("hebrew_file")
+
+    args = parser.parse_args()
+
+    with open(args.english_file) as en, open(args.hebrew_file) as he:
+        diff(en.read(), he.read())
+
+if __name__ == "__main__":
+    main()
