c454c6d576b6d9269579cceae869147473012718
liliya.galina <liliya.galina@neadevis.com>
2020-11-09 17:30:44 +0200

    Add server actions

---
+++ web/static/server-actions.js
@@ -0,0 +1,43 @@
+function callCPUUsageInPercents(){
+   return fetch('/cpu', {
+        method: 'GET'
+    });
+}
+
+function callFileProcessing(file){
+    return fetch('/', {
+        method: 'PUT',
+        body: file
+    });
+}
+
+function callTextProcessing(text){
+    return fetch('/text', {
+        method: 'POST',
+        body: {
+            text: text,
+            sourceLang: 'he',
+            destinationLang: 'en',
+            version: 'some string'
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+}
+
+function callSaveAsTable(textInput, textOutput){
+    return fetch('/save-as-table', {
+        method: 'POST',
+        body: {
+            textInput: textInput,
+            textOutput: textOutput,
+            sourceLang: 'he',
+            destinationLang: 'en',
+            version: 'some string'
+        },
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    });
+}
